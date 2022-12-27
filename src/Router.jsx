568091a8798446a69c14e3e9956634ab8b59c66c
choongmankim <cm730712@gmail.com>
2022-12-27 16:44:30 +0900

    Add: ddd

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,6 +4,7 @@ import Mainpage from './pages/Mainpage';
 import Login from './pages/Login';
 import Mypage from './pages/Mypage';
 import Navigation from './layout/Navigation';
+import Curation from './pages/Curation';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
           <Route path="/" element={<Mainpage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/mypage" element={<Mypage />} />
+          <Route path="/curation" element={<Curation />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Curation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Curation.module.scss';
+
+const Curation = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.innerBox}>
+        <h1 className={classes.title}>내 취향의 카페 찾기</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Curation;
