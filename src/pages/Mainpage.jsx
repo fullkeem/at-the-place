c0dd8f96892848c2e533854690455a0c8e9a49ce
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-12-27 13:51:02 +0900

    [Add][COD-46] 슬라이드 로직 구현 메인페이지 슬라이드 로고 구현

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -4,8 +4,8 @@ import Slide from '../layout/Slide';
 function Mainpage() {
   return (
     <div>
-      <Slide title={'제목 234'} text={'내용 123'} />
-      <Slide title={'제목 435'} text={'내용 432'} />
+      <Slide title={'제목 234'} text={'내용 123'} bg_color={'black'} />
+      <Slide title={'제목 435'} text={'내용 432'} bg_color={'blue'} />
     </div>
   );
 }
