2e3921627ba9c7d79356b891ee0dad9202899621
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-12-27 19:53:06 +0900

    [COD-46][Add] 두번째 슬라이드 구현 동영상 첨부된 슬라이드 적용 z-index 사용용

---
+++ src/components/Mainpage/Slides/FirstSlide.jsx
@@ -0,0 +1,26 @@
+import classes from './FirstSlide.module.scss';
+
+const Slide = (props) => {
+  // let color;
+
+  // if (props.index === 0) {
+  //   color = 'rgb(6, 4, 6)';
+  // } else if (props.index === 1) {
+  //   color = 'rgb(236, 224, 206)';
+  // } else if (props.index === 2) {
+  //   color = 'rgb(255, 0, 10)';
+  // }
+
+  // console.log(props);
+
+  return (
+    <>
+      <div className={classes.slide_wrapper}>
+        <h1>{props.title}</h1>
+        <p>{props.text}</p>
+      </div>
+    </>
+  );
+};
+
+export default Slide;
--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
-import Slide from '../components/Mainpage/Slide';
+import FirstSlide from '../components/Mainpage/Slides/FirstSlide';
+import SecondSlide from '../components/Mainpage/Slides/SecondSlide';
 
-const DUMMY_Data = [
+const data = [
   {
     id: 1,
     title: '지금 잠이 오시나요 충만님?',
     text: '저는 왜 잠이 오는거죠!',
     img: 'url1',
   },
-  { id: 2, title: '제목 5678', text: '내용 5678', img: 'url2' },
+  {
+    id: 2,
+    title: '이거 아닌거 같은데',
+    text: '뭐 방법이 생각 안나니까 이렇게 하죠',
+    img: 'url2',
+  },
   { id: 3, title: '제목 9012', text: '내용 9012', img: 'url3' },
 ];
 
 function Mainpage() {
-  const slideData = DUMMY_Data.map((data, i) => (
-    <Slide
-      key={data.id}
-      title={data.title}
-      text={data.text}
-      img={data.img}
-      index={i}
-    />
-  ));
+  // const slideData = DUMMY_Data.map((data, i) => (
+  //   <Slide
+  //     key={data.id}
+  //     title={data.title}
+  //     text={data.text}
+  //     img={data.img}
+  //     index={i}
+  //   />
+  // ));
 
   return (
-    <div>
-      <ul>{slideData}</ul>
-    </div>
+    <>
+      <FirstSlide title={data[0].title} text={data[0].text} />
+      <SecondSlide title={data[1].title} text={data[1].text} />
+    </>
   );
 }
 
