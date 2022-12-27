42b50b2076754507c1ccab6d6af8202559c0bc5f
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-12-27 15:47:02 +0900

    [COD-46][Mod]슬라이드  로직 수정 슬라이드 색상적용 로직 기존 컬러 값 text 입력 현 rgb 값 인식 가능

--- src/layout/Slide.jsx
+++ src/layout/Slide.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 
 import classes from './Slide.module.scss';
 
 const Slide = (props) => {
+  let color;
+
+  if (props.index === 0) {
+    color = 'rgb(6, 4, 6)';
+  } else if (props.index === 1) {
+    color = 'rgb(236, 224, 206)';
+  } else if (props.index === 2) {
+    color = 'rgb(255, 0, 10)';
+  }
+
   console.log(props);
 
   return (
     <>
       <div
-        className={classes.slide_wrapper}
-        style={{ backgroundColor: `${props.bg_color}` }}
+        className={`${classes.slide_wrapper}`}
+        style={{ backgroundColor: `${color}` }}
       >
         <h1>{props.title}</h1>
         <p>{props.text}</p>
