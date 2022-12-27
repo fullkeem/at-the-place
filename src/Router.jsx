91a38d2b43ae46521c0529bfac693ab8ed511ea3
KimYuHyun <103613612+SeanKim05@users.noreply.github.com>
2022-12-27 13:50:05 +0900

    Basic (#6)
    * [COD-42][Fix] SASS dependencies 찾지 못함  에러
    npm i --save sass- dev 명령어로 sass 재설치
    
    * [COD-41][Add] 기본셋업 추가 작업 시작
    1. 브라우저 로고 작업
    2. 네비게이션 바 로고 삽입
    3. 폰트 import 설정
    4. 불필요한 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from './pages/Main';
+
+import Mainpage from './pages/Mainpage';
 import Login from './pages/Login';
 import Mypage from './pages/Mypage';
+import Navigation from './layout/Navigation';
+
 
 const Router = () => {
   return (
     <>
       <BrowserRouter>
+
+        <Navigation />
         <Routes>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Mainpage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/mypage" element={<Mypage />} />
         </Routes>
---
+++ src/pages/Mainpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Slide from '../layout/Slide';
+
+function Mainpage() {
+  return (
+    <div>
+      <Slide title={'제목 234'} text={'내용 123'} />
+      <Slide title={'제목 435'} text={'내용 432'} />
+    </div>
+  );
+}
+
+export default Mainpage;
