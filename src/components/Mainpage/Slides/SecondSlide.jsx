aca989418c1d352943290905db96cdd56b01b742
KimYuHyun <103613612+SeanKim05@users.noreply.github.com>
2022-12-29 11:23:18 +0900

    Feature/mainpage3 (#9)
    * pull develop
    
    * [COD-46][Add] Logo style
    Logo scroll 이벤트에 따라 opacity 적용
    
    * [COD-46][Add] 로고 스크롤 이벤트
    스크롤에 따른 Opacity 값 적용
    
    * [COD-46][Add] 두번째 슬라이드 구현
    동영상 첨부된 슬라이드 적용 z-index 사용용
    
    * [COD-46][Mod] 슬라이드 순서 변경
    동영상 슬라이드를 첫번째 슬라이드로 이동
    Nav 바 스타일 추가 백그라운드 컬러 스크롤 시 변경

--- src/components/Mainpage/Slides/SecondSlide.jsx
+++ src/components/Mainpage/Slides/SecondSlide.jsx
@@ -1,14 +1,10 @@
 import classes from './SecondSlide.module.scss';
-import Video from '../../../assets/Coffee.mp4';
 
 const SecondSlide = (props) => {
   return (
     <div className={classes.slide_wrapper}>
       <h1>{props.title}</h1>
       <p>{props.text}</p>
-      <video loop autoPlay muted>
-        <source src={Video} type="video/mp4" />
-      </video>
     </div>
   );
 };
--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import FirstSlide from '../components/Mainpage/Slides/FirstSlide';
 import SecondSlide from '../components/Mainpage/Slides/SecondSlide';
+import ThirdSlide from '../components/Mainpage/Slides/ThirdSlide';
 
 const data = [
   {
     id: 1,
     title: '지금 잠이 오시나요 충만님?',
     text: '저는 왜 잠이 오는거죠!',
-    img: 'url1',
   },
   {
     id: 2,
     title: '이거 아닌거 같은데',
     text: '뭐 방법이 생각 안나니까 이렇게 하죠',
-    img: 'url2',
   },
-  { id: 3, title: '제목 9012', text: '내용 9012', img: 'url3' },
+  {
+    id: 3,
+    title: '@thePlace',
+    text: '아니 그런데 말이에요 명조체 느낌이 이상한데?',
+  },
 ];
 
 function Mainpage() {
@@ -33,6 +36,7 @@ function Mainpage() {
     <>
       <FirstSlide title={data[0].title} text={data[0].text} />
       <SecondSlide title={data[1].title} text={data[1].text} />
+      <ThirdSlide title={data[2].title} text={data[2].text} />
     </>
   );
 }
