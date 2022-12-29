41b5b482b522feaf7897619bdbc9854a9e88d672
choongmankim <cm730712@gmail.com>
2022-12-29 14:24:21 +0900

    Add: curation page 소개 글 data 배열로 생성

--- src/pages/Curation.jsx
+++ src/pages/Curation.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import CurationForm from '../components/Auth/CurationPage/CurationForm';
 
+const curationInfoDate = [
+  {
+    id: 1,
+    class: 'classes.timer',
+    img: 'src/assets/oneMinute.png',
+    alt: '60초 타이머',
+    firstP: '질문은 4개.',
+    secondP: '1분이면 OK!',
+  },
+  {
+    id: 2,
+    class: 'classes.check',
+    img: 'src/assets/check.png',
+    alt: '체크표시',
+    firstP: '빠르고,',
+    secondP: '간단하게!',
+  },
+  {
+    id: 3,
+    class: 'classes.donut',
+    img: 'src/assets/donut.png',
+    alt: '커피와 도넛',
+    firstP: '취향에 맞게 답변을 고르면,',
+    secondP: '내가 찾던 카페가 딱!',
+  },
+];
+
 const Curation = () => {
   return (
     <>
-      <CurationForm />
+      <CurationForm curationInfoDate={curationInfoDate} />
     </>
   );
 };
