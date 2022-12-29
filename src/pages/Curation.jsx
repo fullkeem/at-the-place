21a0bf6e5ad1de4d21b0a307467187296c1f7751
choongmankim <cm730712@gmail.com>
2022-12-29 11:24:36 +0900

    Add: curation page 설명글 작성

--- src/pages/Curation.jsx
+++ src/pages/Curation.jsx
@@ -5,7 +5,13 @@ const Curation = () => {
   return (
     <div className={classes.container}>
       <div className={classes.innerBox}>
-        <h1>내 취향의 카페 찾기</h1>
+        <h1 className={classes.title}>내 취향의 카페 찾기</h1>
+        <ul>
+          <li>
+            <p>질문은 4개4️⃣</p>
+            <p>1분이면 OK!</p>
+          </li>
+        </ul>
       </div>
     </div>
   );
