7f8e86de726ba894215a8f6de0330d176e4ee6ef
choongmankim <cm730712@gmail.com>
2022-12-29 11:34:56 +0900

    Fix: curation 페이지 파일 구조 변경

---
+++ src/components/Auth/CurationPage/CurationForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './CurationForm.module.scss';
+
+const CurationForm = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.innerBox}>
+        <h1 className={classes.title}>내 취향의 카페 찾기</h1>
+        <ul>
+          <li>
+            <p>질문은 4개4️⃣</p>
+            <p>1분이면 OK!</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default CurationForm;
--- src/pages/Curation.jsx
+++ src/pages/Curation.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import classes from './Curation.module.scss';
+import CurationForm from '../components/Auth/CurationPage/CurationForm';
 
 const Curation = () => {
   return (
-    <div className={classes.container}>
-      <div className={classes.innerBox}>
-        <h1 className={classes.title}>내 취향의 카페 찾기</h1>
-        <ul>
-          <li>
-            <p>질문은 4개4️⃣</p>
-            <p>1분이면 OK!</p>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <>
+      <CurationForm />
+    </>
   );
 };
 
