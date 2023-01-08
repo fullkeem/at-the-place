edf41f2c2ddfae09e316b6e50f5f967b59885279
choongmankim <cm730712@gmail.com>
2023-01-08 22:31:48 +0900

    Add: questionList 생성

---
+++ src/components/CurationPage/QuestionsList.jsx
@@ -0,0 +1,48 @@
+const QuestionsList = [
+  {
+    id: 1,
+    question: '누구누구님이 카페에서 주로 주문하는 음료는?',
+    answer: [
+      {
+        a: '커피 좀 마신다면 당연히 아메리카노지!',
+        b: '핫한 카페에는 꼭 있는 시그니처 커피!',
+      },
+    ],
+    img: '/src/assets/coffee-love.png',
+  },
+  {
+    id: 2,
+    qusetion: '누구누구님이 좋아하는 분위기의 카페는?',
+    answer: [
+      {
+        a: '힙한 분위기로 가득 찬 힙스터 카페',
+        b: '고즈넉하고 편안한 분위기의 카페',
+      },
+    ],
+    img: '/src/assets/UnboxingDoodle.png',
+  },
+  {
+    id: 3,
+    question: '누구누구님의 디저트 취향은?',
+    answer: [
+      {
+        a: '케이크',
+        b: '여러가지 베이커리',
+      },
+    ],
+    img: '/src/assets/IceCreamDoodle.png',
+  },
+  {
+    id: 4,
+    question: '누구누구님이 주로 카페를 가는 목적은?',
+    answer: [
+      {
+        a: '작업을 할 수 있는 공간이 있는 카페',
+        b: '친구들과 수다도 떨고 사진도 찍기 좋은 카페',
+      },
+    ],
+    img: '/src/assets/ReadingSideDoodle.png',
+  },
+];
+
+export default QuestionsList;
